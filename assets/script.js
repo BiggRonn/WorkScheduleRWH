5d3a2213dc2baeae6595bb33e31babc267d03f1b
Wes Howard <BigRonald42@hotmail.com>
2021-03-05 18:06:18 -0700

    formatted time, adding classes to display

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    var currentDay = $("#currentDay");
+    var timeBlocks = $("#timeBlocks");
+    
+    currentDay.text(moment().format("dddd, MMMM Do, YYYY h:mmA"));
+
+
+
+
+})
+
