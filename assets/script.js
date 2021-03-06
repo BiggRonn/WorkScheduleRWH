1970a34cd52b983af417dfc3a97a7cd1e66747c1
Wes Howard <BigRonald42@hotmail.com>
2021-03-06 12:23:42 -0700

    vars for indiv time

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,44 @@
 $(document).ready(function(){
 
     var currentDay = $("#currentDay");
-    var timeBlocks = $("#timeBlocks");
+    var time9 = $("#9");
+    var time10 = $("#10");
+    var time11 = $("#11");
+    var time12 = $("#12");
+    var time13 = $("#13");
+    var time14 = $("#14");
+    var time15 = $("#15");
+    var time16 = $("#16");
+    var time17 = $("#17");
     
     currentDay.text(moment().format("dddd, MMMM Do, YYYY h:mmA"));
+
+    
+
+    time9.text("")
+    console.log(time9);
+    var blah = document.querySelector("9");
+    console.log(blah)
+
+    time9.addEventListener("click", typeText);
+
+
+
+
+
+    
+
+
 
 
 
 
 })
+
+function typeText(){
+
+
+}
+
+
 
