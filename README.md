cdc379549fd41cd652082d3ec96f82e5b495189c
Wes Howard <BigRonald42@hotmail.com>
2021-03-08 20:30:17 -0700

    save functions with text for all times

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler
 
-# Link to deployed site
+Allows user to input text in time blocks for each hour in the workday. Clicking the save button next to the text will store the text in local storage. Loading the page again with items in the local storage will display in the boxes. 
+
+
 https://biggronn.github.io/WorkScheduleRWH/
 
 <img src="assets/wsdcap.PNG" alt= "screen capture of deployed site">
--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,17 @@ $(document).ready(function(){
 
     var currentDay = $("#currentDay");
     $("#9").val(localStorage.getItem("9"));
-    var time10 = $("#10");
-    var time11 = $("#11");
-    var time12 = $("#12");
-    var time13 = $("#13");
-    var time14 = $("#14");
-    var time15 = $("#15");
-    var time16 = $("#16");
-    var time17 = $("#17");
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#13").val(localStorage.getItem("13"));
+    $("#14").val(localStorage.getItem("14"));
+    $("#15").val(localStorage.getItem("15"));
+    $("#16").val(localStorage.getItem("16"));
+    $("#17").val(localStorage.getItem("17"));
     
-    time10.val("")
+    
+    
     currentDay.text(moment().format("dddd, MMMM Do, YYYY h:mmA"));
 
     console.log(localStorage.getItem("9"));
@@ -28,7 +29,7 @@ $(document).ready(function(){
     
     //time9.addClass("bg-danger");
 
- 
+    
 
 
 
@@ -48,8 +49,8 @@ $(document).ready(function(){
 /*
 
 
-if moment().format("military hour") >,<, = this.id {
-    this.setAttribute("background-color: past, present, future")
+if moment().format("H") >,<, = this.id {
+    this.addClass("background-color: past, present, future")
 }
 
 */
